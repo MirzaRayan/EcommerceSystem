1bc1172f12779e322a400d15d7b05d7322744911
app <app@Mirza-Rayan.bbrouter>
2026-04-30 16:00:32 +0500

    updateCategory controller added

--- src/controllers/category.controllers.js
+++ src/controllers/category.controllers.js
@@ -45,7 +45,66 @@ const createCategory = async (req, res) => {
     }
 }
 
+const updateCategory = async (req, res) => {
+    try {
+
+        const category = await Category.findById(req.params.id);
+
+
+        if(!category) {
+            return res.status(404).json({
+                message: 'Category doesNot exists'
+            })
+        }
+        
+
+        const key = Object.keys(req.body);
+
+        const allowedFields = ['name','description'];
+
+        const isValid = key.every((fields) => allowedFields.includes(fields))
+
+        if(!isValid) {
+            return res.status(400).json({
+                message: 'You cannot update this field'
+            })
+        }
+
+
+        if(req.body.name) {
+            const existedCategory = await Category.findOne({
+                name: req.body.name
+            });
+
+            if(existedCategory) {
+                return res.status(400).json({
+                    message: 'Category with this name already exists'
+                })
+            }
+        }
+
+
+        const updatedCategory = await Category.findByIdAndUpdate(
+            category._id,
+            req.body,
+            { new: true }
+        )
+
+        return res.status(200).json({
+            message: 'Category updated successfully',
+            data: updatedCategory
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'Server error while updating category'
+        })
+    }
+}
+
 
 export {
     createCategory,
+    updateCategory,
 }
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/Auth.middleware.js";
 import { isAdmin }  from '../middlewares/Admin.middleware.js'
-import { createCategory } from "../controllers/category.controllers.js";
+import { createCategory, updateCategory } from "../controllers/category.controllers.js";
 
 const router = Router();
 
 
 // protected routes
 router.route('/createCategory').post(verifyJWT, isAdmin, createCategory)
+router.route('/update/:id').put(verifyJWT, isAdmin, updateCategory)
 
 
 
