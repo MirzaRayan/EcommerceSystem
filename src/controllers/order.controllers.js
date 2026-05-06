ea41aeaa2b233f62720ee866c6ddeec00b2f0a3e
app <app@Mirza-Rayan.bbrouter>
2026-05-06 15:24:40 +0500

    getSingleOrder controller added

--- src/controllers/order.controllers.js
+++ src/controllers/order.controllers.js
@@ -91,12 +91,47 @@ const getMyOrders = async (req, res) => {
     }
 }
 
+const getSingleOrder = async (req, res) => {
+    try {
 
+        const order = await Order.findById(req.params.id)
+
+        if(!order) {
+            return res.status(404).json({
+                message: 'Order not found'
+            })
+        }
+
+        if(order.userId.toString() !== req.user._id.toString()) {
+            return res.status(403).json({
+                message: 'You are not allowed to view this order'
+            })
+        }
+
+        const orderItems = await OrderItems.find({ 
+            orderId: order._id 
+        }).populate('productId', 'name price image')
+
+        return res.status(200).json({
+            message: 'Order fetched successfully',
+            data: {
+                order,
+                orderItems
+            }
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'Server Error while getting order'
+        })
+    }
+}
 
 
 
 export {
     placeOrder,
     getMyOrders,
-
+    getSingleOrder
 }
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/Auth.middleware.js";
-import { getMyOrders, placeOrder } from "../controllers/order.controllers.js";
+import { getMyOrders, getSingleOrder, placeOrder } from "../controllers/order.controllers.js";
 
 const router = Router();
 
 
 router.route('/placeOrder').post(verifyJWT, placeOrder)
 router.route('/getMyOrder').get(verifyJWT, getMyOrders)
+router.route('/getSingleOrder/:id').get(verifyJWT, getSingleOrder)
 
 
 
